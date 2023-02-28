5dec2f43ce5e17a35ce3ddf3f4e615c05ec3a79f
ksnd0297 <ksnd0297@gmail.com>
2023-03-01 00:21:26 +0900

    fetures/thunk

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom';
 import Menu from './components/Menu';
 import RedPage from './pages/RedPage';
 import BluePage from './pages/BluePage';
+import UsersPage from './pages/UsersPage';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/red" element={<RedPage />} />
         <Route path="/blue" element={<BluePage />} />
+        <Route path="/users" element={<UsersPage />} />
       </Routes>
     </div>
   );
---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,23 @@
+import {useEffect} from 'react';
+import Users from '../components/Users';
+import {connect} from 'react-redux';
+import {getUsers} from '../modules/users';
+
+const UsersContainer = ({users, getUsers}) => {
+  // 컴포넌트가 마운트되고 나서 호출
+  useEffect(() => {
+    if (users) return;
+    getUsers();
+  }, [users, getUsers]);
+
+  return <Users users={users} />;
+};
+
+export default connect(
+  (state) => ({
+    users: state.users.users,
+  }),
+  {
+    getUsers,
+  }
+)(UsersContainer);
